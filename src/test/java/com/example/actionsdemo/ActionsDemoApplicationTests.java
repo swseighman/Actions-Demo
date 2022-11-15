09dfae09b1a5e2c57829935880e68ae1680fc82f
swseighman <scottseighman@gmail.com>
2022-11-15 13:39:20 -0500

    Initial commit

---
+++ src/test/java/com/example/actionsdemo/ActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actionsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
